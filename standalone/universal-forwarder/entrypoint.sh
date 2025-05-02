8f647ba4af12ec7077a9bc189ab9e8d34c9e63de
ofadiman <ofadiman@gmail.com>
2025-05-02 10:42:52 +0200

    docs(standalone): document script for generating stable client names based on container names

--- standalone/universal-forwarder/entrypoint.sh
+++ standalone/universal-forwarder/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# This script creates a stable `clientName` in the form of UUID v5 for each universal forwarder based on its container name, which is assigned by docker.
+
 set -euo pipefail
 
+# A container can find its name by querying the Docker API using its ID.
 CONTAINER_NAME=$(curl --unix-socket /var/run/docker.sock "http://localhost/containers/$(basename $(cat /proc/1/cpuset))/json" | jq -r '.Name')
 
-CLIENT_ID=$(uuidgen --sha1 --namespace @oid --name "${CONTAINER_NAME}")
+# Generate a stable client name from the container name.
+CLIENT_NAME=$(uuidgen --sha1 --namespace @oid --name "${CONTAINER_NAME}")
 
-echo "clientName = ${CLIENT_ID}" >>"${SPLUNK_HOME}/etc/system/local/deploymentclient.conf"
+echo "clientName = ${CLIENT_NAME}" >>"${SPLUNK_HOME}/etc/system/local/deploymentclient.conf"
 
 exec "$@"
